b8630e104d067d62ab7f01bb7fd435b2e1a1089f
Vinayaka-Voleti <vinayaka.vvk@gmail.com>
2024-08-04 12:30:30 +0530

    Modify the text they way you like using the buttons.

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import "./App.css";
 import NavBar from "./Components/NavBar";
 import TextArea from "./Components/TextArea";
 import Alert from "./Components/Alert";
-import About from "./Components/About";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+// import About from "./Components/About";
+// import {
+  // BrowserRouter as Router,
+  // Routes,
+  // Route
+// } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
@@ -39,16 +39,19 @@ function App() {
   };
 
   return (
-    <Router>
+    // <Router>
+    <>
       <NavBar title="Home" terms="Terms" policy="About" mode={mode} DarkMode={DarkMode} />
       <Alert alert={alert} />
       <div className="container my-3">
-        <Routes>
-          <Route exact path="/about" element={<About mode={mode} DarkMode={DarkMode}/>} />
-          <Route exact path="/" element={<TextArea showAlert={showAlert} label="TextArea:- Enter Your Text" mode={mode} DarkMode={DarkMode} />} />
-        </Routes>
+        {/* <Routes> */}
+          {/* <Route exact path="/about" element={<About mode={mode} DarkMode={DarkMode}/>} /> */}
+          {/* <Route exact path="/" element={<TextArea showAlert={showAlert} label="TextArea:- Enter Your Text" mode={mode} DarkMode={DarkMode} />} /> */}
+          <TextArea showAlert={showAlert} label="TextArea:- Enter Your Text" mode={mode} DarkMode={DarkMode} />
+        {/* </Routes> */}
       </div>
-    </Router>
+    {/* </Router> */}
+    </>
   );
 }
 
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 export default function NavBar(props)  {
     return(
         
 <nav className={`navbar navbar-expand-lg bg-${props.mode} navbar-${props.mode}`}>
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/"></Link>
+    <a className="navbar-brand" href="#"></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link" aria-current="page" to="/">{props.title}</Link>
+          <a className="nav-link" aria-current="page" href="#">{props.title}</a>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/">{props.terms}</Link>
+          <a className="nav-link" href="#">{props.terms}</a>
         </li>
         {/* <li className="nav-item dropdown" id="Dropped">
           <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -29,9 +29,9 @@ export default function NavBar(props)  {
             <li><a className="dropdown-item" href="/">Something else here</a></li>
           </ul> 
         </li> */}
-        <li className="nav-item">
+        {/* <li className="nav-item">
           <Link className="nav-link" to="/about" >{props.policy}</Link>
-        </li>
+        </li> */}
       </ul>
       {/* <button id="button">Login</button> */}
       
